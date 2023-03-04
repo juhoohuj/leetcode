dd8d5c2c535e41aa82e3741cb9c3da838eb81299
Juho Ahonen <juho.ahonen2@gmail.com>
2023-03-04 19:43:49 +0200

    .

--- 169.js
+++ 169.js
@@ -3,22 +3,18 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    let biggest = 0
-    let count = 0
+    let b = 0
+    let c = 0
     let d = new Set(nums)
     let u = [...d]
-
-
-    let duplicates = u.map(value => [value, nums.filter(str => str === value).length]);
-    
+    let duplicates = u.map(value => [value, nums.filter(str => str === value).length]);    
     duplicates.forEach(e => {
-        if(e[1] > count) {
-            biggest = e[0]
-            count = e[1]
+        if(e[1] > c) {
+            b = e[0]
+            c = e[1]
         }
     });
-
-    return biggest
+    return b
 };
 
 
